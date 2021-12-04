ef5eb1427dd0f02428af8306dc18bd1a0d32b4ae
Omar Naman <onaman@uwaterloo.ca>
2021-12-04 13:07:51 -0500

    add Q1 P1

---
+++ 1/part1/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+#ifdef TEST
+    freopen("test_input.in", "r", stdin);
+#elif REAL
+    freopen("real_input.in", "r", stdin);
+#endif
+    int prev_depth = -1, cur_depth = -1;
+    cin >> prev_depth;
+    int res = 0;
+    while (cin >> cur_depth)
+    {
+        if (cur_depth > prev_depth)
+        {
+            res++;
+        }
+        prev_depth = cur_depth;
+    }
+    cout << res << endl;
+}
