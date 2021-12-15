e3708081bdc6d38ffae54d7ce2fdd7afade2ebe2
Omar Naman <onaman@uwaterloo.ca>
2021-12-15 10:28:24 -0500

    add Q6

---
+++ 6/part1/src/main.rs
@@ -0,0 +1,27 @@
+use std::fs;
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let file_path = &args[1];
+    let contents = fs::read_to_string(file_path).expect("File not found");
+    let fishes = contents.split(',');
+    let mut res: Vec<u32> = vec![0; 9];
+
+    for i in fishes.into_iter() {
+        res[i.parse::<usize>().unwrap()] += 1;
+    }
+    for _ in 0..80 {
+        let mut temp_res: Vec<u32> = vec![0; 9];
+        for i in 0..res.len() {
+            if i == 0 {
+                temp_res[8] = res[0];
+                temp_res[6] += res[0];
+            } else {
+                temp_res[i - 1] += res[i];
+            }
+        }
+        res = temp_res.clone();
+    }
+    println!("res: {}", res.iter().sum::<u32>())
+}
